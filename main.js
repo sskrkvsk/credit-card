3dc84f3d130326e0b99a372591392ea6779d3b96
ssk <s99ruy@gmail.com>
2024-01-21 12:42:41 +0100

    card number function 95% perfection

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     const inputElement = document.getElementById('creditCardInput');
-  
-    inputElement.addEventListener('input', function(e) {
-      let inputValue = inputElement.value.replace(/[^0-9]/g, ''); // Remove non-numeric characters
-      
-      // Handle backspace
-      if (e.inputType === 'deleteContentBackward' || e.inputType === 'deleteContentForward') {
-      
-          inputValue = inputValue.slice(0, -1); // Remove the last character (hyphen)
+    inputElement.addEventListener('keydown', function(event) {
+      if (event.key === '-') {
+        event.preventDefault();}});
 
-      } else {
-        // Add hyphen after every 4 digits
-        inputValue = inputValue.replace(/(\d{4})/g, '$1-');
-      }
-  
-      if (inputValue.length <= 16) {
+    inputElement.addEventListener('input', function(e) {
+      let inputValue = inputElement.value.replace(/[^0-9-]/g, '');
+
+      if (inputValue.length > 0) {
+        if (inputValue.length === 4 || inputValue.length === 9 || inputValue.length === 14) {
+          inputValue += "-";
+          if (e.inputType === 'deleteContentBackward' || e.inputType === 'deleteContentForward') {
+            inputValue = inputValue.slice(0, -1);}}}
+
+      const withoutHyphen = inputValue.replace(/[^0-9]/g, '');
+      if (withoutHyphen.length <= 16) {
         inputElement.value = inputValue;
+      } else {
+        inputElement.value = withoutHyphen.slice(0, 16);
       }
-  
       inputValue.length === 20 ? inputElement.classList.add('valid') : inputElement.classList.remove('valid');
-      console.log(inputValue.length);
     });
   });
   
